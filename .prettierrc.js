5640309346ff54df4ce5152cde35435c35df8cc2
Jihong Min <weed_001@Naver.com>
2024-03-15 01:03:37 +0900

    prettierrc 설정 추가

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  singleQuote: false,
+  semi: false,
+  tabWidth: 2,
+  trailingComma: "all",
+  printWidth: 80,
+  bracketSpacing: true,
+  arrowParen: "always",
+  endOfLine: "auto",
+}
